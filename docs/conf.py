80ee9dc1b015107214f9298e6eae1654bcafccfe
Daniel Ryan <dryan@dryan.com>
2013-10-17 12:26:35 -0400

    conf.py theme options

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,11 @@ html_theme = 'flask_small'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "github_fork": "dryan/d3ploy",
+    "index_logo": None,
+    "index_logo_height": 0
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes', 'docs/_themes']
