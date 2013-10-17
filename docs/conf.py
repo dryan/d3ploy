5368b21bf2210a4b5d6ea72f7abd70848c4df009
Daniel Ryan <dryan@dryan.com>
2013-10-17 12:10:57 -0400

    tweaking makefile

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,10 @@
 # serve to show the default.
 
 import sys, os
-sys.path.append(os.path.abspath('_themes'))
+if os.getcwd() == 'docs':
+    sys.path.append(os.path.abspath('_themes'))
+else:
+    sys.path.append(os.path.abspath('docs/_themes'))    
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
