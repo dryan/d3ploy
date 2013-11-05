00025621772d1ce4d7ed16467c8d339e1e6d72c4
Daniel Ryan <dryan@dryan.com>
2013-11-05 11:38:38 -0500

    switching to MIT License

--- d3ploy.py
+++ d3ploy.py
@@ -2,7 +2,7 @@
 
 # Notification Center code borrowed from https://github.com/maranas/pyNotificationCenter/blob/master/pyNotificationCenter.py
 
-VERSION =   '2.0.2'
+VERSION =   '2.0.3'
 
 import os, sys, json, re, hashlib, argparse, urllib, time, base64, ConfigParser, gzip, mimetypes, zipfile, signal, Queue, threading
 from xml.dom import minidom
