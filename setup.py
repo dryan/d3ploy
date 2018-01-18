c62390ebd95e26ccb1200f26fbcaa9b57f5657fc
Daniel Ryan <dryan@dryan.com>
2018-01-18 15:18:47 -0500

    pypi distribution setup. how did I never commit this before now?

---
+++ setup.py
@@ -0,0 +1,29 @@
+import re
+from distutils.core import setup
+
+VERSION_FINDER = re.compile(
+    r'^VERSION = \'([\d+]\.[\d+].[\d+])\'$',
+    re.MULTILINE
+)
+
+script = open('d3ploy/d3ploy.py', 'r').read()
+
+VERSION = VERSION_FINDER.findall(script)
+
+if VERSION:
+    VERSION = VERSION.pop()
+else:
+    raise ValueError('Could not find version number in script file')
+
+setup(
+    name='d3ploy',
+    packages=['d3ploy'],
+    version=VERSION,
+    description='Script for uploading files to S3 with multiple environment support.',
+    author='Dan Ryan',
+    author_email='d@dryan.com',
+    url='https://github.com/dryan/d3ploy',
+    download_url='https://github.com/dryan/d3ploy/archive/{}.tar.gz'.format(
+        VERSION),
+    scripts=['bin/d3ploy'],
+)
