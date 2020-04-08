406238694bd344c0d707b328af42eb019be24b7e
dryan <dryan@users.noreply.github.com>
2020-04-08 09:16:05 -0400

    reformatting setup.py

--- setup.py
+++ setup.py
@@ -3,36 +3,34 @@ import re
 from distutils.core import setup
 
 VERSION_FINDER = re.compile(
-    r'^VERSION = \'([\d+]\.[\d+].[\d+](-beta[\d+]?)?)\'$',
-    re.MULTILINE
+    r"^VERSION = \'([\d+]\.[\d+].[\d+](-beta[\d+]?)?)\'$", re.MULTILINE
 )
 
-script = open('d3ploy/d3ploy.py', 'r').read()
+script = open("d3ploy/d3ploy.py", "r").read()
 
 VERSION = VERSION_FINDER.findall(script)
 
 if VERSION:
     VERSION = VERSION.pop()[0]
 else:
-    raise ValueError('Could not find version number in script file')
+    raise ValueError("Could not find version number in script file")
 
-DESCRIPTION = ''
+DESCRIPTION = ""
 
-if os.path.exists('README.md'):
-    DESCRIPTION = open('README.md', 'r').read()
-if os.path.exists('README.rst'):
-    DESCRIPTION = open('README.rst', 'r').read()
+if os.path.exists("README.md"):
+    DESCRIPTION = open("README.md", "r").read()
+if os.path.exists("README.rst"):
+    DESCRIPTION = open("README.rst", "r").read()
 
 setup(
-    name='d3ploy',
-    packages=['d3ploy'],
+    name="d3ploy",
+    packages=["d3ploy"],
     version=VERSION,
-    description='Script for uploading files to S3 with multiple environment support.',
+    description="Utility for uploading files to S3 with multiple environment support.",
     long_description=DESCRIPTION,
-    author='Dan Ryan',
-    author_email='d@dryan.com',
-    url='https://github.com/dryan/d3ploy',
-    download_url='https://github.com/dryan/d3ploy/archive/{}.tar.gz'.format(
-        VERSION),
-    scripts=['bin/d3ploy'],
+    author="Dan Ryan",
+    author_email="opensource@dryan.com",
+    url="https://github.com/dryan/d3ploy",
+    download_url="https://github.com/dryan/d3ploy/archive/{}.tar.gz".format(VERSION),
+    scripts=["bin/d3ploy"],
 )
