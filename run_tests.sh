0c4ee10b687c546d47d7f0473ae36779ebcc0077
dryan <dryan@users.noreply.github.com>
2020-04-08 09:13:26 -0400

    switch tests to use poetry

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-pipenv run coverage erase
-pipenv run coverage run --source='d3ploy' tests/test.py "$@"
-pipenv run coverage html
-pipenv run coverage report --fail-under=100 --skip-covered
+poetry run coverage erase
+poetry run coverage run --source='d3ploy' tests/test.py "$@"
+poetry run coverage html
+poetry run coverage report --fail-under=100 --skip-covered
