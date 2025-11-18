cc81cb4e9065af0a981e7457ee288183b373ef03
Dan Ryan (GitHub Noreply Key) <dryan@users.noreply.github.com>
2025-11-18 13:47:30 -0500

    ♻️ Use named constants for cache durations
    - Add NO_CACHE, ONE_DAY, ONE_YEAR constants
    - Improves readability and maintainability

--- d3ploy/config/constants.py
+++ d3ploy/config/constants.py
@@ -2,12 +2,17 @@
 Configuration constants.
 """
 
+# Cache durations in seconds
+NO_CACHE = 0
+ONE_DAY = 86400
+ONE_YEAR = 31536000
+
 RECOMMENDED_CACHES = {
-    "text/html": 0,
-    "text/css": 31536000,
-    "text/javascript": 31536000,
-    "application/json": 31536000,
-    "application/manifest+json": 86400,
-    "image/*": 31536000,
-    "font/*": 31536000,
+    "text/html": NO_CACHE,
+    "text/css": ONE_YEAR,
+    "text/javascript": ONE_YEAR,
+    "application/json": ONE_YEAR,
+    "application/manifest+json": ONE_DAY,
+    "image/*": ONE_YEAR,
+    "font/*": ONE_YEAR,
 }
